7716ca17f23b7e677bdb984f915e56ee9346c9d1
felixlyh <felixyhlui@gmail.com>
2023-04-10 22:30:19 +0800

    menu UI Update

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,25 +1,123 @@
 #include <iostream>
+#include <fstream>
+#include <ncurses.h>
+#include <curses.h>
+#include <string>
+//#include <windows.h>
+#include <unistd.h> //,replace windows.h for linux
+#include <stdio.h>
 using namespace std;
 
+int fileline(string filename) {
+	int nol=0;
+	ifstream target;
+	string lines;
+	target.open(filename);
+	if (target.fail()) {
+		cout << "ERROR: Fail to open the file" << endl;
+		return 0;
+	}
+	while (getline(target,lines)) {
+		nol += 1;
+	}
+	target.close();
+	return nol;
+}
+
+void mainmenu_display (int select) {
+	int xMax,yMax,nol;
+    int xCursor, yCursor;
+	// ifstream art;
+	// string logo;
+	string menuitem[10] = {
+		"**********************************", 
+		" Welcome to Pac-Man Alpha Edition ", 
+		"**********************************",
+		"",
+		"Play The Pac-Man Game !",
+		"View The Game Statistics",
+		"View The Staff Credits",
+		"Exit The Main Menu",
+		"",
+		"Please expand your terminal for a better game experience!",
+	};
+
+	getmaxyx(stdscr,yMax,xMax);
+
+	// art.open("./ascii_art/pacman_title.txt");
+	// nol = fileline("./ascii_art/pacman_title.txt");
+	// xCursor = (xMax/2);
+    // yCursor = (yMax/2)-menuitem->length()/2-nol-1;
+	// move(yCursor, xCursor);
+
+	
+	// while (getline(art,logo)) {
+	// 	xCursor = (xMax/2) - logo.length()/2;
+	// 	move(yCursor++, xCursor);
+	// 	addstr(logo.c_str());
+		
+	// }
+	
+	attron(COLOR_PAIR(1));
+	for (int i = 0; i < 10; i++) {
+		if (i-4 == select) {
+			attron(A_REVERSE);
+		}
+		xCursor = (xMax/2) - menuitem[i].length()/2;
+		yCursor = (yMax/2) - menuitem->length()/2 + i;
+		move(yCursor,xCursor);
+		addstr(menuitem[i].c_str());
+		if (i-4 == select) {
+			attroff(A_REVERSE);
+		}
+	}
+	attroff(COLOR_PAIR(1));
+
+	refresh();
+}
+
 int main(){
-	cout << "    ***********************************" << endl;
-	cout << "      Welcome to Pac-Man Alpha Edition " << endl;
-	cout << "    ***********************************" << endl;
-	cout << "	" << endl;
-	cout << "		Please input your option :" << endl;
-	cout << "	1. Play The Pac-Man Game !" << endl;
-	cout << "	2. View The Game Statistics" << endl;
-	cout << "	3. View The Staff Credits" << endl;
-	cout << "	4. Exit The Main Menu" << endl;
-	cout << "	" << endl;
-	cout << "* Expand your terminal for a better game experience!" << endl;
-	cout << "	" << endl;
-	cout << " Input your option (1/2/3/4) :";
-	int option;
-	cin >> option;
-	if ((option>=1) && (option<=4))
-		cout << "Valid input, " << option << endl;
-	else
-		cout << "Invalid input, " << option << endl;
+	initscr();
+	start_color();
+	if(can_change_color()) {
+        init_color(COLOR_BLACK,0,0,0);
+        init_color(COLOR_CYAN,0,0,888);
+    }
+	init_pair(1,COLOR_YELLOW,COLOR_CYAN);
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, TRUE);
+
+	int selection = 0;
+
+	while (true) {
+		mainmenu_display(selection);
+		int cinput = getch();
+		if (cinput == '\n') {
+			if (selection == 0){
+				//enter the main game
+				break;
+			}else if (selection == 1) {
+				// Load save
+				break;
+			}else if (selection == 2) {
+				// load  Credit Screen
+				break;
+			}else if (selection == 3) {
+				// exit the game
+				break;
+			}
+		} else if (cinput == KEY_DOWN && selection < 3) {
+			selection++;
+		} else if (cinput== KEY_UP && selection > 0) {
+			selection--;
+		}
+		usleep(150000);
+	}
+
+	getch();
+	endwin();
+
 	return 0;
 }

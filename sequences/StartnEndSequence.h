654d381645e8e7243a238f0a87c919aadd01674b
lokwy-hku <125896717+lokwy-hku@users.noreply.github.com>
2023-05-06 17:43:10 +0800

    Update StartnEndSequence.h

--- sequences/StartnEndSequence.h
+++ sequences/StartnEndSequence.h
@@ -8,8 +8,8 @@ int StartingSequence();
 int EndingSequence();
 void howToPlay();
 void loadStatistics(int score, string name, int highscore);
-void CheckScore(int score, int highscore);
-void loadprofile(int highscore, string name);
+void CheckScore(int score, int &highscore);
+void loadprofile(int &highscore, string &name);
 void exportprofile(int highscore, string name);
 int GameOver(int score);
 
